2cbf5978f290036c005a4fd2b231c48c41c2032a
saidzahedi <saeedzahedi644@yahoo.com>
2022-06-20 09:12:25 +0430

    PrivateTournoment class made

---
+++ src/main/java/ir/ac/kntu/AllPrivateTournoment.java
@@ -0,0 +1,7 @@
+package ir.ac.kntu;
+
+import java.util.ArrayList;
+
+public class AllPrivateTournoment {
+    public static ArrayList<PrivateTournoment>privateTournoments=new ArrayList<>();
+}
---
+++ src/main/java/ir/ac/kntu/PrivateTournoment.java
@@ -0,0 +1,23 @@
+package ir.ac.kntu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PrivateTournoment {
+    private String name;
+    private ArrayList<Users>members=new ArrayList<>();
+    private ArrayList<Question>questions=new ArrayList<>();
+    private int Max_Number=20;
+    private ArrayList<String>History=new ArrayList<>();
+    private HashMap<Question, HashMap<Users,Double>>Final=new HashMap<>();
+    private boolean AbleTogetnewMember=true;
+    public PrivateTournoment(String name) {
+        this.name = name;
+    }
+    public void checktogetnewMember(){
+        if(members.size()<=Max_Number){
+            this.AbleTogetnewMember=false;
+        }
+    }
+
+}
